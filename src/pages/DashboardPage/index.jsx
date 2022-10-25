e468207ad456678fc03a7bf41241415864141470
sanddlekfx <sanddlexown@hotmail.com>
2022-10-25 18:40:55 -0300

    feat: adicionado botão com a função de logout

--- src/pages/DashboardPage/index.jsx
+++ src/pages/DashboardPage/index.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./style.css";
 
+import { AuthContext } from '../../contexts/auth'
+
 const DashboardPage = () => {
-  return <div>Dashboard</div>;
+  const { logout } = useContext(AuthContext);
+
+  const handleLogout = () => {
+    logout();
+  }
+
+  return (
+    <div className='header-dashboard'>
+      Dashboard
+      <button onClick={handleLogout}>
+        Sair
+      </button>
+    </div>
+  );
 };
 
 export default DashboardPage;
